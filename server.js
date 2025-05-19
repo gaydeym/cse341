8e91ff997da0c0a44d85a2428e02cdf4f7be33d7
gaydeym <gay20001@byui.com>
2025-05-19 00:01:33 -0500

    extra credit

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+app.use(bodyParser.json()).use('/', require('./routes'));
 
 mongodb.initDb((err) => {
   if (err) {
